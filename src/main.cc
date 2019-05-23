7704cb10f97e8cd3cacb426108af6741d6662768
Izzy Axel <izzoriousaxel@gmail.com>
2019-05-22 20:55:33 -0400

    Refactor, updates

--- src/main.cc
+++ src/main.cc
@@ -6,24 +6,24 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Invalid number of arguments provided, usage is 'primepatcher.exe [path to main.dol]'\nPress enter to exit");
-		std::cin.get();
+		std::cin.ignore();
 		return 1;
 	}
 	FILE *input = fopen(argv[1], "rb+");
 	if(!input)
 	{
 		printf("An error occurred while trying to read file %s\nPress enter to exit", argv[1]);
-		std::cin.get();
+		std::cin.ignore();
 		return 1;
 	}
 	int const val1 = 0x39, val2 = 0xf3, val3 = 0xde, val4 = 0x28;
-	fseek(input, 130846, std::ios::beg);
+	fseek(input, 130846, SEEK_SET);
 	fwrite(&val1, 1, 1, input);
-	fseek(input, 130847, std::ios::beg);
+	fseek(input, 130847, SEEK_SET);
 	fwrite(&val2, 1, 1, input);
-	fseek(input, 130858, std::ios::beg);
+	fseek(input, 130858, SEEK_SET);
 	fwrite(&val3, 1, 1, input);
-	fseek(input, 130859, std::ios::beg);
+	fseek(input, 130859, SEEK_SET);
 	fwrite(&val4, 1, 1, input);
 	fclose(input);
 	printf("main.dol has been patched");
